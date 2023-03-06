4223e8325a56477cecbc16273c5e3d73c96e54a7
David-YY-Berger <91850832+David-YY-Berger@users.noreply.github.com>
2023-03-06 11:59:28 +0200

    Update Main.tcl
    not yet finished! just a test!

--- Exercise 0/Main.tcl
+++ Exercise 0/Main.tcl
@@ -1,54 +1,70 @@
 #!tclsh
 
 
-proc handleBuy {line} {
-   set result "### BUY "
-   append result [lindex $line 1]
-   append result " ###\n"
+proc handleBuyAndPrint {line fh} {
+    #returns value added in this line, 
+    #and prints the finished line to console and file
+   set PrintResult "### BUY "
+   append PrintResult [lindex $line 1]
+   append PrintResult " ###\n"
    set mult [expr { [lindex $line 2] * [lindex $line 3] }]
-   append result $mult
-   return $result
+   append PrintResult $mult
+   puts $PrintResult
+   puts $fh $PrintResult
+   return $mult
 
 }
 
-proc handleSell {line} {
-   set result "### SELL "
-   append result [lindex $line 1]
-   append result " ###\n"
+proc handleSellAndPrint {line fh} {
+    #returns value added in this line, 
+    #and prints the finished line to console and file
+   set PrintResult "$$$ SELL "
+   append PrintResult [lindex $line 1]
+   append PrintResult " $$$\n"
    set mult [expr { [lindex $line 2] * [lindex $line 3] }]
-   append result $mult
-   return $result
+   append PrintResult $mult
+   puts $PrintResult
+   puts $fh $PrintResult
+   return $mult
 
 }
-
-#open file for writing
-set output [ open "Tar0.txt" w]
 
 #set path to folder, might need to this by getting input from user
-set path "C:/Users/yehuda/Workspace/TCLproject"
+set inputPath "C:/TCL/Targil0/" ; #you might have to switch "\" to "/"
+set outputPath "C:/TCL/Targil0/outputFolder/";
 
+#open file for writing
+set outputFileName "Tar0.asm"
+set output [ open $outputPath$outputFileName w]
 
 # scan through the files in the path and find vm files
-set files [glob $path/*.vm]
+set files [glob $inputPath/*.vm]
+set totalBuy 0
+set totalSell 0
+set temp 0
+
 foreach file $files {
     set fh [open $file r]
-    puts $fh
+    puts [file tail $file]:\n
+    puts $output [file tail $file]:\n
 	
-    # visualize the data on screen of both files
+    # iterate thru files:
     while { [gets $fh var ] >= 0 } { 
-        if {[lindex $var 0] == "buy"} {
-            puts [handleBuy $var]
-            puts $output [handleBuy $var]
-        } elseif {[lindex $var 0] == "cell"} {
-            puts [handleSell $var]
-            puts $output [handleSell $var]
-        }
+	if {[lindex $var 0] == "buy"} {
+	    set temp [handleBuyAndPrint $var $output]
+       set totalBuy [expr {$totalBuy + $temp}] 
+	} elseif {[lindex $var 0] == "cell"} {
+	    set temp [handleSellAndPrint $var $output]
+        set totalSell [expr {$totalSell + $temp}]
+	}
     }
-
-	
-    #insert logic for program
-    
-	
-close $fh
+    puts "\n\n"
+    puts $output "\n\n"
+    close $fh
 }
+puts TotalBuys$totalBuy\n
+puts $output TotalBuys$totalBuy\n
+puts TotalSells$totalSell\n
+puts $output TotalSells$totalSell\n
+close $output
 
