6eae78803a8bb0911c207b1daf5e43b184b4fcb3
David-YY-Berger <davidberger2448@gmail.com>
2023-04-25 14:18:59 +0300

    began Ex 2, test fails, must check why

--- Exercise 2/Main.tcl
+++ Exercise 2/Main.tcl
@@ -1,9 +1,13 @@
 #   bs"d
 #   Course: Fundamentals of S/W Languages - 150060.5783
 #   Submitters: Yehuda Shani 23794253 & David Berger 341441053
-#   Exercise: 2
+#   Exercise: 1
 #   Language: TCL
 #   Lecturer: Y. Barzilly
+
+# This is the main part of the program, it will open the input and output files, call the parser
+# and code writer
+
 
 package require itcl
 
@@ -12,24 +16,29 @@ source "parser.tcl"
 source "codeWriter.tcl"
 
 #(1) initializes instances of our helper classes:
-parser pars input.vm
-codeWriter code output.asm
+parser pars BasicLoop.vm
+codeWriter code BasicLoop.asm
 
 #(2) converting vm code to asm code:
 while {[pars hasMoreCommands]} {
     pars advance
     #set local variable "operation" to the value of the "commandType" field from the parser object
     set operation [pars info variable commandType -value]
-    
+    set line [pars info variable command -value]
+
     if {$operation eq "C_PUSH" || $operation eq "C_POP"} {
-        code writePushPop [pars info variable command -value]
+        code writePushPop $line input.vm
     } elseif {$operation eq "C_ARITHMETIC"} {
-        code writeArithmetic [pars info variable command -value]
+        code writeArithmetic $line
     } elseif {$operation eq "C_LABEL"} {
         code writeLabel [pars info variable command -value]
     } elseif {$operation eq "C_GOTO"} {
         code writeGoTo [pars info variable command -value]
     } elseif {$operation eq "C_IF"} {
         code writeIfGoTo [pars info variable command -value]
-    } 
+    }
 }
+
+#(3) closing all files:
+pars closeFile
+code closeFile
--- Exercise 2/codeWriter.tcl
+++ Exercise 2/codeWriter.tcl
@@ -1,17 +1,26 @@
 package require itcl
 
 itcl::class codeWriter {
-    variable outputFile
+    variable outputFile ; # file handler..
+    variable outputFileName ; # actual name, used to write labels
+    variable labelCounter 0
 
     constructor {fileName} {
+        set outputFileName $fileName
         set outputFile [open $fileName w]
     }
 
+    method closeFile { } {
+        close $outputFile
+    }
+    
     method writeArithmetic {command} {
+        variable labelCounter
+        set labelCounter [expr $labelCounter + 1]
         set operation [lindex $command 0]
         if {$operation eq "add" || $operation eq "sub" || $operation eq "and" || $operation eq "or" } {
             puts $outputFile "@SP"
-            puts $outputFile "AM=M-1"
+            puts $outputFile "AM=M-1" ;
             puts $outputFile "D=M"
             puts $outputFile "A=A-1"
             if {$operation eq "add"} {
@@ -23,7 +32,7 @@ itcl::class codeWriter {
             } elseif {$operation eq "or"} {
                 puts $outputFile "M=M|D"
             }
-        } 
+        }
         if {$operation eq "neg" || $operation eq "not"} {
             puts $outputFile "@SP"
             puts $outputFile "A=M-1"
@@ -39,7 +48,7 @@ itcl::class codeWriter {
             puts $outputFile "D=M"
             puts $outputFile "A=A-1"
             puts $outputFile "D=M-D"
-            puts $outputFile "@TRUE$operation"
+            puts $outputFile "@TRUE$operation-$labelCounter"
             if {$operation eq "eq"} {
                 puts $outputFile "D;JEQ"
             } elseif {$operation eq "gt"} {
@@ -50,41 +59,37 @@ itcl::class codeWriter {
             puts $outputFile "@SP"
             puts $outputFile "A=M-1"
             puts $outputFile "M=0"
-            puts $outputFile "@END$operation"
+            puts $outputFile "@END$operation-$labelCounter"
             puts $outputFile "0;JMP"
-            puts $outputFile "(TRUE$operation)"
+            puts $outputFile "(TRUE$operation-$labelCounter)"
             puts $outputFile "@SP"
             puts $outputFile "A=M-1"
             puts $outputFile "M=-1"
-            puts $outputFile "(END$operation)"
+            puts $outputFile "(END$operation-$labelCounter)"
         }
 
     }
 
-    method writePushPop {command} {
+    method writePushPop {command fileName} {
         #parse the given command into 3 parts. Example of a command: "push constant 10"
-        set operation [lindex $command 0] 
-        set segment [lindex $command 1] 
+        set operation [lindex $command 0]
+        set segment [lindex $command 1]
         set index [lindex $command 2]
 
+        set firstOptions {"local" "argument" "this" "that"}
+
         if {$operation eq "push"} {
-            if {$segment eq "constant"} {
-                puts $outputFile "@$index"
-                puts $outputFile "D=A"
-                puts $outputFile "@SP"
-                puts $outputFile "A=M"
-                puts $outputFile "M=D"
-                puts $outputFile "@SP"
-                puts $outputFile "M=M+1"
-            }
-            elseif {$segment eq "local" || $segment eq "argument" || $segment eq "this" || $segment eq "that"} {
+            if {$segment in $firstOptions} {
                 if {$segment eq "local"} {
                     puts $outputFile "@LCL"
-                } elseif {$segment eq "argument"} {
+                }
+                if {$segment eq "argument"} {
                     puts $outputFile "@ARG"
-                } elseif {$segment eq "this"} {
+                }
+                if {$segment eq "this"} {
                     puts $outputFile "@THIS"
-                } elseif {$segment eq "that"} {
+                }
+                if {$segment eq "that"} {
                     puts $outputFile "@THAT"
                 }
                 puts $outputFile "D=M"
@@ -96,8 +101,8 @@ itcl::class codeWriter {
                 puts $outputFile "M=D"
                 puts $outputFile "@SP"
                 puts $outputFile "M=M+1"
-            }
-            elseif {$segment eq "temp"} {
+            } elseif {$segment eq "temp"} {
+                #temp segment starts at 5
                 puts $outputFile "@5"
                 puts $outputFile "D=A"
                 puts $outputFile "@$index"
@@ -108,8 +113,26 @@ itcl::class codeWriter {
                 puts $outputFile "M=D"
                 puts $outputFile "@SP"
                 puts $outputFile "M=M+1"
-            }
-            elseif {$segment eq "pointer"} {
+
+            } elseif {$segment eq "constant"} {
+                puts $outputFile "@$index"
+                puts $outputFile "D=A"
+                puts $outputFile "@SP"
+                puts $outputFile "A=M"
+                puts $outputFile "M=D"
+                puts $outputFile "@SP"
+                puts $outputFile "M=M+1"
+
+            } elseif {$segment eq "static"} {
+                puts $outputFile "@$fileName.$index"
+                puts $outputFile "D=M"
+                puts $outputFile "@SP"
+                puts $outputFile "A=M"
+                puts $outputFile "M=D"
+                puts $outputFile "@SP"
+                puts $outputFile "M=M+1"
+
+            } elseif {$segment eq "pointer"} {
                 if {$index eq "0"} {
                     puts $outputFile "@THIS"
                 } elseif {$index eq "1"} {
@@ -121,24 +144,21 @@ itcl::class codeWriter {
                 puts $outputFile "M=D"
                 puts $outputFile "@SP"
                 puts $outputFile "M=M+1"
-            } elseif {$segment eq "static"} {
-                puts $outputFile "@$fileName.$index"
-                puts $outputFile "D=M"
-                puts $outputFile "@SP"
-                puts $outputFile "A=M"
-                puts $outputFile "M=D"
-                puts $outputFile "@SP"
-                puts $outputFile "M=M+1"
             }
+
+
         } elseif {$operation eq "pop"} {
-            if {$segment eq "local" || $segment eq "argument" || $segment eq "this" || $segment eq "that"} {
+            if {$segment in $firstOptions} {
                 if {$segment eq "local"} {
                     puts $outputFile "@LCL"
-                } elseif {$segment eq "argument"} {
+                }
+                if {$segment eq "argument"} {
                     puts $outputFile "@ARG"
-                } elseif {$segment eq "this"} {
+                }
+                if {$segment eq "this"} {
                     puts $outputFile "@THIS"
-                } elseif {$segment eq "that"} {
+                }
+                if {$segment eq "that"} {
                     puts $outputFile "@THAT"
                 }
                 puts $outputFile "D=M"
@@ -152,6 +172,7 @@ itcl::class codeWriter {
                 puts $outputFile "@R13"
                 puts $outputFile "A=M"
                 puts $outputFile "M=D"
+
             } elseif {$segment eq "temp"} {
                 puts $outputFile "@5"
                 puts $outputFile "D=A"
@@ -165,6 +186,14 @@ itcl::class codeWriter {
                 puts $outputFile "@R13"
                 puts $outputFile "A=M"
                 puts $outputFile "M=D"
+
+            } elseif {$segment eq "static"} {
+                puts $outputFile "@SP"
+                puts $outputFile "AM=M-1"
+                puts $outputFile "D=M"
+                puts $outputFile "@$fileName.$index"
+                puts $outputFile "M=D"
+
             } elseif {$segment eq "pointer"} {
                 puts $outputFile "@SP"
                 puts $outputFile "AM=M-1"
@@ -175,24 +204,29 @@ itcl::class codeWriter {
                     puts $outputFile "@THAT"
                 }
                 puts $outputFile "M=D"
-            } elseif {$segment eq "static"} {
-                puts $outputFile "@SP"
-                puts $outputFile "AM=M-1"
-                puts $outputFile "D=M"
-                puts $outputFile "@$fileName.$index"
-                puts $outputFile "M=D"
             }
         }
     }
-
+    
     method writeLabel {command} {
-        
+        set newLabel [lindex $command 1]
+        puts $outputFile "($outputFileName.$newLabel)"
     }
-    method writeGoTo {command}{
-        
+    method writeGoTo {command} {
+        set newLabel [lindex $command 1]
+        puts $outputFile "@$outputFileName.$newLabel"
+        puts $outputFile "0; JMP"
     }
-    method writeIfGoTo {command}{
-        
+    method writeIfGoTo {command} {
+        set newLabel [lindex $command 1]
+        puts $outputFile "@SP"
+        puts $outputFile "M=M-1"
+        puts $outputFile "A=M"
+        puts $outputFile "D=M"
+        puts $outputFile "@$outputFileName.$newLabel"
+        puts $outputFile "D;JNE"
     }
 
+    
+    #end of class
 }
